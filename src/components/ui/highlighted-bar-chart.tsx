d2d002e8c77fd53e64bea58b1c4f20c9923d61e3
ExploitEngineer <revilwhoami0098@gmail.com>
2025-09-30 02:00:58 +0500

    feat(ui): add reusable UI components

---
+++ src/components/ui/highlighted-bar-chart.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { Bar, BarChart, XAxis, Cell } from "recharts";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Separator } from "@/components/ui/separator";
+import { useTranslations } from "next-intl";
+
+const chartData = [
+  { month: "January", desktop: 342 },
+  { month: "February", desktop: 876 },
+  { month: "March", desktop: 512 },
+  { month: "April", desktop: 629 },
+  { month: "May", desktop: 458 },
+  { month: "June", desktop: 781 },
+  { month: "July", desktop: 394 },
+  { month: "August", desktop: 925 },
+  { month: "September", desktop: 647 },
+  { month: "October", desktop: 532 },
+  { month: "November", desktop: 803 },
+  { month: "December", desktop: 271 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+} satisfies ChartConfig;
+
+export function HighlightedBarChart() {
+  const t = useTranslations("main-dashboard");
+  const [activeIndex, setActiveIndex] = React.useState<number | null>(null);
+
+  const activeData = React.useMemo(() => {
+    if (activeIndex === null) return null;
+    return chartData[activeIndex];
+  }, [activeIndex]);
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex w-full items-center justify-between">
+          <CardTitle>{t("dashboard-page.chart.primary-title")}</CardTitle>
+          <CardTitle className="font-light">
+            {t("dashboard-page.chart.secondary-title")}
+          </CardTitle>
+        </div>
+        <Separator className="my-2" />
+        <CardDescription>
+          {activeData
+            ? `${activeData.month}: ${activeData.desktop}`
+            : "January - June 2025"}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            onMouseLeave={() => setActiveIndex(null)}
+          >
+            <rect
+              x="0"
+              y="0"
+              width="100%"
+              height="85%"
+              fill="url(#highlighted-pattern-dots)"
+            />
+            <defs>
+              <DottedBackgroundPattern />
+            </defs>
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey="desktop" radius={4} fill="var(--color-desktop)">
+              {chartData.map((_, index) => (
+                <Cell
+                  className="duration-200"
+                  key={`cell-${index}`}
+                  fillOpacity={
+                    activeIndex === null ? 1 : activeIndex === index ? 1 : 0.3
+                  }
+                  stroke={activeIndex === index ? "var(--color-desktop)" : ""}
+                  onMouseEnter={() => setActiveIndex(index)}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
+
+const DottedBackgroundPattern = () => {
+  return (
+    <pattern
+      id="highlighted-pattern-dots"
+      x="0"
+      y="0"
+      width="10"
+      height="10"
+      patternUnits="userSpaceOnUse"
+    >
+      <circle
+        className="dark:text-muted/40 text-muted"
+        cx="2"
+        cy="2"
+        r="1"
+        fill="currentColor"
+      />
+    </pattern>
+  );
+};
