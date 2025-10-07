95790812f150211ef994e7c6dce6c2dc64b2ab94
ExploitEngineer <revilwhoami0098@gmail.com>
2025-10-07 23:00:03 +0500

    feat(auth): add new logo in auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { LangSwitcher } from "@/components/lang-switcher";
 import { ModeToggle } from "@/components/theme-toggle";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function AuthLayout({
@@ -7,21 +10,22 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { resolvedTheme } = useTheme();
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-100 px-4 py-6 dark:bg-zinc-900">
       <div className="mb-6 flex w-full max-w-lg items-center justify-between">
         <div className="flex items-center gap-2">
           <Image
-            src="/assets/images/bh-logo.png"
-            width={40}
-            height={40}
+            src={
+              resolvedTheme === "dark"
+                ? "/assets/images/dark-logo.png"
+                : "/assets/images/logo.png"
+            }
+            width={150}
+            height={150}
             alt="company logo"
-            className="sm:h-[50px] sm:w-[50px]"
+            priority
           />
-          <div className="flex items-center font-bold sm:text-lg">
-            <span>budgethub</span>
-            <span className="text-blue-600">.ch</span>
-          </div>
         </div>
 
         <div className="flex items-center gap-2">
